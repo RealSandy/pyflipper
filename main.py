9910a60c5ebc4dddc808cab9126ff92e1d404c50
RealSandy <reuben@twoshoes.org.uk>
2023-10-26 18:39:57 +0100

    Api key removed, more work done...

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import pyflipper_functions
 
-API_KEY = pyflipper_functions.import_api_key("config.json")
-BAZAAR_DATA = pyflipper_functions.get_bazaar_data(API_KEY)
+ITEMS_DATA = pyflipper_functions.get_items_data()
+BAZAAR_DATA = pyflipper_functions.get_bazaar_data()
 BAZAAR_LAST_UPDATED = pyflipper_functions.return_bazaar_datetime(BAZAAR_DATA)
 BAZAAR_PRODUCT_INFO = pyflipper_functions.return_bazaar_product_info(BAZAAR_DATA)
 BAZAAR_PRODUCTS = []
 
 for product in BAZAAR_PRODUCT_INFO:
-    BAZAAR_PRODUCTS.append(pyflipper_functions.BazaarItem(BAZAAR_PRODUCT_INFO[product]["quick_status"]))
+    BAZAAR_PRODUCTS.append(pyflipper_functions.BazaarItem(BAZAAR_PRODUCT_INFO[product]["quick_status"], ITEMS_DATA))
+
+viable_flips = list(filter(lambda x: x.margin <= 56, BAZAAR_PRODUCTS))
+viable_flips.sort(key=lambda x: x.margin, reverse=False)
+
+for i in range(len(viable_flips)):
+    print(f"#{len(viable_flips) - i} {viable_flips[i]}")
 
 print(f"Last updated: {BAZAAR_LAST_UPDATED}")
 
--- pyflipper_functions.py
+++ pyflipper_functions.py
@@ -1,39 +1,80 @@
-import json
+# import json
 import requests
 import datetime
 
 
 class BazaarItem:
-    def __init__(self, product_data):
+    def __init__(self, product_data, items_data):
+        self.ITEMS_DATA = items_data
         self.product_data = product_data
+
         self.item_id = self.product_data['productId']
+        self.name = self.get_name_from_id()
+
+        self.sell_order_price = self.product_data['buyPrice']
+        self.buy_order_price = self.product_data['sellPrice']
+
+        self.display_sell_order_price = add_commas_to_number(int(self.sell_order_price * 100) / 100)
+        self.display_buy_order_price = add_commas_to_number(int(self.buy_order_price * 100) / 100)
+
+        self.buy_volume = add_commas_to_number(self.product_data['buyVolume'])
+        self.sell_volume = add_commas_to_number(self.product_data['sellVolume'])
+
+        self.profit = self.calculate_profit()
         self.margin = self.calculate_margin()
-        self.buy_volume = self.product_data['buyVolume']
-        self.sell_volume = self.product_data['sellVolume']
-        print(self.product_data, "\n", self.margin, self.buy_volume, self.sell_volume)
+
+        self.display_profit = add_commas_to_number(int(self.profit * 100) / 100)
+        self.display_margin = int(self.margin * 100) / 100
+
+        # print(self.item_id, self.name)
+        # print(self.product_data, "\n", self.margin, self.buy_volume, self.sell_volume)
+        # print(self)
 
     def calculate_margin(self):
-        sell_order_price = self.product_data["buyPrice"]
-        buy_order_price = self.product_data["sellPrice"]
-        if int(buy_order_price == 0 or sell_order_price == 0):
+        if int(self.buy_order_price == 0 or self.sell_order_price == 0):
             margin = 0
         else:
-            profit = sell_order_price - buy_order_price
-            margin = (profit * 100) / sell_order_price
+            margin = (self.profit * 100) / self.sell_order_price
         return margin
 
+    def calculate_profit(self):
+        if int(self.buy_order_price == 0 or self.sell_order_price == 0):
+            profit = 0
+        else:
+            profit = self.sell_order_price - self.buy_order_price
+        return profit
 
-def import_api_key(path_to_file):
-    f = open(path_to_file, "r")
-    data = json.load(f)
-    return data['api_key']
+    def get_name_from_id(self):
+        if self.item_id.startswith("ENCHANTMENT"):
+            name = self.item_id
+            name = name.lower()
+            name = name.replace("_", " ")
+            name = name.replace("enchantment ", "")
+            name += " Book"
+            name = name.title()
+            name = name.replace("Bane Of Arthropods", "Bane of Arthropods")
+            return name
+        for material in self.ITEMS_DATA['items']:
+            if material['id'] == self.item_id:
+                return material['name']
+
+    def __str__(self):
+        string = f"""{self.name}: 
+    Buy Order Price: {self.display_buy_order_price} coins
+    Buy Volume: {self.buy_volume}
+    Sell Order Price: {self.display_sell_order_price} coins
+    Sell Volume: {self.sell_volume}
+    Profit per Item: {self.display_profit} coins
+    Profit Margin: {self.display_margin}%
+"""
+        return string
 
 
-def get_bazaar_data(api_key):
-    r = requests.get(f"https://api.hypixel.net/skyblock/bazaar?key={api_key}")
+def get_bazaar_data():
+    r = requests.get("https://api.hypixel.net/skyblock/bazaar")
     data = r.json()
     if not data['success']:
-        raise Exception("Sorry, but the API request failed")
+        raise Exception("Sorry, but the API request failed. Try again later.")
     return data
 
 
@@ -46,3 +87,19 @@ def return_bazaar_datetime(data):
 def return_bazaar_product_info(data):
     info = data['products']
     return info
+
+
+def get_items_data():
+    r = requests.get("https://api.hypixel.net/resources/skyblock/items")
+    data = r.json()
+    if not data['success']:
+        raise Exception("Sorry, but the API request failed. Try again later.")
+    return data
+
+
+def add_commas_to_number(number):
+    return remove_trailing_zeroes(format(number, ",f"))
+
+
+def remove_trailing_zeroes(string):
+    return string.rstrip("0").rstrip(".")
